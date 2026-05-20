cf421a3b7d8afbaab51065766776e5f80908a664
vivekkandpal81 <vivekkandpal81@gmail.com>
2026-05-20 21:00:06 +0530

    add urls

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,8 @@ const port = process.env.PORT || 4000;
 
 // ⭐ IMPORTANT: ENABLE CREDENTIALS FOR CLERK COOKIE SESSION
 const allowedOrigins = [
-  "http://localhost:5173", // user frontend
-  "http://localhost:5174", // admin dashboard
+  "https://medicare-frontend-657g.onrender.com", // user frontend
+  "https://medicare-admin-2n7c.onrender.com", // admin dashboard
 ];
 
 app.use(
